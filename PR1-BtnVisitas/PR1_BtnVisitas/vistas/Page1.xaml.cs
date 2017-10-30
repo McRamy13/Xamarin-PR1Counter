45e10d3e7f2bde25bb677f435d9e61fae9659802
Ramón Guardia López <Mc_Ramy13@hotmail.com>
2017-10-30 09:21:31 +0100

    Agregar archivos de proyecto.

---
+++ PR1-BtnVisitas/PR1_BtnVisitas/vistas/Page1.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace PR1_BtnVisitas.Vistas
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class Page1 : ContentPage
+    {
+
+        //contador
+        private int contador = 0;
+        public Page1()
+        {
+            InitializeComponent();
+            //onClicked action
+            btnPressed.Clicked += (sender, args) => {
+                onBtnIncrementPressed();
+            };
+        }
+
+        private void onBtnIncrementPressed()
+        {
+            contador += 1;
+            //change label value
+            if(contador == 1)
+                lblCounter.Text = String.Format("Se ha pulsado el botón {0} vez",contador);
+            else
+                lblCounter.Text = String.Format("Se ha pulsado el botón {0} veces", contador);
+        }
+    }
+}
